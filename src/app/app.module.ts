cda4854d3edabbb79126b34caf647f3ebb723882
mateuszfranke <mateuszfranke@icloud.com>
2020-11-27 09:03:09 +0100

    working on header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { JobComponent } from './jobs/job/job.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobsComponent,
+    JobComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
